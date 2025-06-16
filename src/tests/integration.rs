fdb98dc37d81ecd1ff87ffce1386b8d41f358ba2
norris <defghij@starscourge>
2025-06-15 22:15:02 -0400

    Get a single basic test working

--- src/tests/integration.rs
+++ src/tests/integration.rs
@@ -9,7 +9,10 @@ use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
 
 use crate::{
     dto::dinosaur::CreateDinosaur,
-    persistence::db::create_table,
+    persistence::{
+        db::create_table,
+        DB_URL
+    },
     interface,
     domain::dinosaur::Taxonomy
 };
@@ -17,17 +20,17 @@ use crate::{
 #[tokio::test]
 async fn test_create_and_list_dinosaur() {
     // Set up in-memory database for testing.
-    let pool = SqlitePool::connect("sqlite::memory:").await.expect("DB should already exist or be created");
+    let pool = SqlitePool::connect(DB_URL).await.expect("DB should already exist or be created");
     create_table(&pool).await;
 
     let app = interface::routes(Arc::new(pool));
 
-
-    let payload: CreateDinosaur = CreateDinosaur { 
-        species: String::from("T-rex"),
-        taxonomy: Taxonomy::Genus
-    };
-    let payload = serde_json::json!(payload);
+    let payload = serde_json::json!(
+        CreateDinosaur {
+            species: String::from("T-rex"),
+            taxonomy: Taxonomy::Genus
+        }
+    );
 
     let response = app
         .clone()
@@ -41,26 +44,18 @@ async fn test_create_and_list_dinosaur() {
         )
         .await
         .unwrap();
-
     assert_eq!(response.status(), StatusCode::CREATED);
 
-    //let response = app
-        //.oneshot(
-            //Request::builder()
-                //.method("GET")
-                //.uri("/dinosaurs")
-                //.body(Body::empty())
-                //.unwrap(),
-        //)
-        //.await
-        //.unwrap();
-
-    //assert_eq!(response.status(), StatusCode::OK);
-
-    //let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-    //let json: serde_json::Value = serde_json::from_slice(&body).unwrap();
-
-    //assert!(json.is_array());
-    //assert!(json.as_array().unwrap().iter().any(|d| d["species"] == "Pachycephalosaurus"));
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method("GET")
+                .uri("/dinosaurs")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
 }
 
